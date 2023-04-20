06afa4c2ddfe87cc93e29417f5faea158e8695ee
Blake Tolliver <blaketolliver87@gmail.com>
2023-04-20 11:42:36 -0400

    update css, ready for release

--- index.js
+++ index.js
@@ -5,17 +5,21 @@
 */
 
 const lengthEl = document.getElementById("length-el")
+const lengthElInch = document.getElementById("length-el-inch")
 const volumeEl = document.getElementById("volume-el")
 const massEl = document.getElementById("mass-el")
 let inputEl = document.getElementById("input-el")
 const convertBtn = document.getElementById("convert-btn")
 
+console.log(lengthElInch)
 
 let length = ""
 let volume = ""
 let mass = ""
 
 convertBtn.addEventListener("click", function(){
+    lengthElInch.innerHTML = `
+    ${inputEl.value} millimeters = ${(inputEl.value / 25.4).toFixed(3)} inches | ${inputEl.value} inches = ${(inputEl.value * 25.4).toFixed(3)} millimeters`
     lengthEl.innerHTML = `
     ${inputEl.value} meters = ${(inputEl.value * 3.281).toFixed(3)} feet | ${inputEl.value} feet = ${(inputEl.value / 3.281).toFixed(3)} meters`
     volumeEl.innerHTML = `
